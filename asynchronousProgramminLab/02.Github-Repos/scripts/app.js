7cf14552b4d293d5198ee1f4a7bfa2d86229aa9b
Tsveti1103 <cvstoqnova@gmail.com>
2022-07-04 12:56:21 +0300

    Initial commit

---
+++ asynchronousProgramminLab/02.Github-Repos/scripts/app.js
@@ -0,0 +1,56 @@
+// function loadRepos() {
+// 	const username = document.getElementById('username').value;
+// 	const list = document.getElementById('repos');
+// 	fetch(`https://api.github.com/users/${username}/repos`)
+// 		.then(handleResponse)
+// 		.then(displayRepos)
+// 		.catch(handleError);
+
+// 	function handleResponse(response) {
+// 		if (response.ok === false) {
+// 			throw new Error(`${response.status} ${response.statusText}`);
+// 		}
+// 		return response.json();
+// 	}
+// 	function displayRepos(data) {
+// 		list.innerHTML = ''
+// 		for (let repo of data) {
+// 			list.innerHTML += ` <li>
+//             <a href="${repo.html_url}">
+//                 ${repo.full_name}
+//             </a>
+//         </li>`
+// 		}
+// 	}
+// 	function handleError(error) {
+// 		list.innerHTML = `${error.message}`;
+// 	}
+// }
+
+
+
+async function loadRepos() {
+	const username = document.getElementById('username').value;
+	const list = document.getElementById('repos');
+	
+	try {
+		const response = await fetch(`https://api.github.com/users/${username}/repos`);
+
+		if (response.ok === false) {
+			throw new Error(`${response.status} ${response.statusText}`);
+		}
+		const data = await response.json();
+
+		list.innerHTML = ''
+		for (let repo of data) {
+			list.innerHTML += ` <li>
+            <a href="${repo.html_url}">
+                ${repo.full_name}
+            </a>
+        </li>`
+		}
+	} catch (error) {
+		list.innerHTML = `${error.message}`;
+
+	}
+}
---
+++ asynchronousProgramminLab/03.Gighub-Commits/app.js
@@ -0,0 +1,21 @@
+async function loadCommits() {
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+    const list = document.getElementById('commits');
+    try {
+        const response = await fetch(`https://api.github.com/repos/${username}/${repo}/commits`);
+        if (response.ok == false) {
+            throw new Error(`${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        list.innerHTML = ``
+
+        for(let {commit} of data){
+            list.innerHTML += `<li>${commit.author.name}: ${commit.message}</li>`
+
+        }
+    }catch (err) {
+        list.innerHTML = `Error: ${err.message}`;
+    }
+
+}
