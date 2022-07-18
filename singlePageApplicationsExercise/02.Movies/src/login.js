60909d4e553b9da2ec5558590a04a9d3e9c69110
Tsveti1103 <cvstoqnova@gmail.com>
2022-07-18 14:42:36 +0300

    Create login.js

---
+++ singlePageApplicationsExercise/02.Movies/src/login.js
@@ -0,0 +1,41 @@
+import { showView, updateNav } from "./util.js";
+import { homePage } from "./home.js";
+
+
+const section = document.querySelector('#form-login');
+const form = section.querySelector('form')
+form.addEventListener('submit', onSubmit);
+export function loginPage() {
+    showView(section);
+}
+
+async function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    await login(email, password);
+    form.reset();
+    updateNav()
+    homePage();
+
+}
+
+async function login(email, password) {
+    try {
+        const response = await fetch('http://localhost:3030/users/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        })
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error(error.message)
+        }
+        const user = await response.json();
+        localStorage.setItem('user', JSON.stringify(user));
+    } catch (err) {
+        alert(err.message);
+        throw err;
+    }
+}
