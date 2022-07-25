219bfa6e2f8d24f22bf57d843f6e2c105ca947f4
Tsveti1103 <cvstoqnova@gmail.com>
2022-07-25 14:17:56 +0300

    Create catalog.js

---
+++ examPrep/JSApplicationsRetakeExam16Aug2021/src/views/catalog.js
@@ -0,0 +1,22 @@
+import { html } from '../../node_modules/lit-html/lit-html.js'
+import * as gamesService from '../api/gamesService.js'
+
+const catalogTemplate = (allGames) => html`
+        <section id="catalog-page">
+            <h1>All Games</h1>
+            ${allGames.length>0? allGames.map(previewTemplate):
+            html`<h3 class="no-articles">No articles yet</h3>`}
+        </section>`
+const previewTemplate = (game) => html`
+<div class="allGames">
+<div class="allGames-info">
+    <img src="${game.imageUrl}">
+    <h6>${game.category}</h6>
+    <h2>${game.title}</h2>
+    <a href="/details/${game._id}" class="details-button">Details</a>
+</div>
+</div>`
+export async function catalogPage(ctx) {
+    const allGames = await gamesService.getAll()
+    ctx.render(catalogTemplate(allGames))
+}
