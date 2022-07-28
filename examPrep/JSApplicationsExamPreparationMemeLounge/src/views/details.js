61fa914906ac6e82ec49b8006b1916df380ec70c
Tsveti1103 <cvstoqnova@gmail.com>
2022-07-28 14:23:49 +0300

    Create details.js

---
+++ examPrep/JSApplicationsExamPreparationMemeLounge/src/views/details.js
@@ -0,0 +1,40 @@
+import { html, nothing } from '../../node_modules/lit-html/lit-html.js'
+import * as itemSevice from '../api/itemService.js'
+
+
+const detailsTemplate = (item, onDelete) => html`
+<section id="meme-details">
+    <h1>Meme Title: ${item.title}</h1>
+    <div class="meme-details">
+        <div class="meme-img">
+            <img alt="meme-alt" src="${item.imageUrl}">
+        </div>
+        <div class="meme-description">
+            <h2>Meme Description</h2>
+            <p>${item.description}</p>
+
+            
+        ${item.isOwner?html`
+        <a class="button warning" href="/edit/${item._id}">Edit</a>
+            <button @click=${onDelete} href="javascript:void(0)" class="button danger">Delete</button>
+        `:nothing}
+           
+        </div>
+    </div>
+</section>`
+
+export async function detailsPage(ctx) {
+    const itemId = ctx.params.id;
+    const item = await itemSevice.getById(itemId)
+    if (ctx.user) {
+        item.isOwner = ctx.user._id == item._ownerId;
+    }
+    ctx.render(detailsTemplate(item, onDelete))
+    async function onDelete() {
+        const choice = confirm(`Are you sure you want to delete this ${item.title}?`)
+        if (choice) {
+            await itemSevice.deleteById(itemId);
+            ctx.page.redirect('/catalog')
+        }
+    }
+}
