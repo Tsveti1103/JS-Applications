6547dbe263009519b66cb3235bbfd74ede1d77ad
Tsveti1103 <cvstoqnova@gmail.com>
2022-07-30 14:28:06 +0300

    Create userServiece.js

---
+++ examPrep/JSApplicationsExamPreparation2/src/api/userServiece.js
@@ -0,0 +1,25 @@
+import { clearUserData, setUserData } from '../utils.js';
+import * as api from './api.js';
+
+const endpoints = {
+    login: '/users/login',
+    register: '/users/register',
+    logout: '/users/logout'
+}
+
+export async function login(username, password) {
+    const result = await api.post(endpoints.login,{username, password});
+    setUserData(result);
+    return result;
+}
+
+export async function register(username, password) {
+    const result = await api.post(endpoints.register,{username, password});
+    setUserData(result);
+    return result;
+}
+
+export async function logout() {
+    api.get(endpoints.logout);
+    clearUserData();
+}
