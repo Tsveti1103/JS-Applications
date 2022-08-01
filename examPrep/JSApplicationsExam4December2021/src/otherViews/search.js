5254abf9cf988d9c937276a623d0d280e2afc584
Tsveti1103 <cvstoqnova@gmail.com>
2022-08-01 14:34:21 +0300

    Create search.js

---
+++ examPrep/JSApplicationsExam4December2021/src/otherViews/search.js
@@ -0,0 +1,52 @@
+import { html,nothing } from '../../node_modules/lit-html/lit-html.js'
+import * as itemSevice from '../api/itemService.js'
+
+
+const searchTemplate = (user, allItems,onClick) => html`
+<section id="searchPage">
+<h1>Search by Name</h1>
+<div class="search">
+    <input id="search-input" type="text" name="search" placeholder="Enter desired albums's name">
+    <button @click=${onClick} class="button-list">Search</button>
+</div>
+
+<h2>Results:</h2>
+    <div class="search-result"></div>
+        ${allItems.length > 0 ? allItems.map(item => previewTemplate(user, item)) 
+        : html`<p class="no-result">No result.</p>`}
+    </div>
+</section>`
+
+const previewTemplate = (user, item) => html`
+<div class="card-box">
+    <img src="${item.imgUrl}">
+    <div>
+        <div class="text-center">
+            <p class="name">Name: ${item.name}</p>
+            <p class="artist">Artist: ${item.artist}</p>
+            <p class="genre">Genre: ${item.genre}</p>
+            <p class="price">Price: ${item.price}</p>
+            <p class="date">Release Date: ${item.releaseDate}</p>
+        </div>
+        ${user ? html`
+        <div class="btn-group">
+            <a href="/details/${item._id}" id="details">Details</a>
+        </div>`: nothing}
+
+    </div>
+</div>`
+
+
+
+export  function searchPage(ctx) {
+    const user = ctx.user
+    function onClick(ev){
+        const query = document.getElementById('search-input').value;
+        itemSevice.search(query)
+        .then(allItems => {
+            ctx.render(searchTemplate(user,allItems,onClick))
+        })
+    }
+    ctx.render(searchTemplate(user,[],onClick))
+
+}
